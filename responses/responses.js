1296c3c409715e157639337e5e70de152141e64e
ntsmith7 <nick@polarity.io>
2021-12-29 11:41:14 -0800

    responses

---
+++ responses/responses.js
@@ -0,0 +1,67 @@
+const _ = require("lodash");
+
+const polarityError = (err) => {
+  const errorMsg = err.message;
+  return {
+    detail: errorMsg ? errorMsg : "Unknown Error",
+    error: err,
+  };
+};
+
+const emptyResponse = (entity) => [
+  {
+    entity,
+    data: null,
+  },
+];
+
+const polarityResponse = (entity, response, Logger) => {
+  Logger.trace({ RESPONSE: response });
+  const results = _.get(response, "alerts.entries");
+
+  return _.get(response, "alerts.entries").length
+    ? [
+        {
+          entity,
+          data: results
+            ? { summary: getSummary(response), details: response }
+            : null,
+        },
+      ]
+    : emptyResponse(entity);
+};
+
+const retryablePolarityResponse = (entity) => {
+  return [
+    {
+      entity,
+      isVolatile: true,
+      data: {
+        summary: ["Lookup limit reached"],
+        details: {
+          summaryTag: "Lookup limit reached",
+          errorMessage:
+            'A temporary FireEye HX API search limit was reached. You can retry your search by pressing the "Retry Search" button.',
+        },
+      },
+    },
+  ];
+};
+
+const getSummary = (response) => {
+  let tags = [];
+  tags.push(`Alerts: ${response.alerts.entries.length}`);
+
+  for (const alert of response.alerts.entries) {
+    tags.push(`IOC: ${alert.indicator.name}`);
+  }
+
+  return tags;
+};
+
+module.exports = {
+  polarityError,
+  emptyResponse,
+  polarityResponse,
+  retryablePolarityResponse,
+};
